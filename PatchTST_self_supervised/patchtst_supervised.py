f036f3ae93a53c74e922762e952591cc4e987945
Zeeshan Ahmad <zeeshanahmad10809@gmail.com>
2025-09-30 03:58:27 +0200

    added new args as per need

--- PatchTST_self_supervised/patchtst_supervised.py
+++ PatchTST_self_supervised/patchtst_supervised.py
@@ -21,17 +21,17 @@ import argparse
 
 parser = argparse.ArgumentParser()
 # Dataset and dataloader
-parser.add_argument('--dset', type=str, default='etth1', help='dataset name')
-parser.add_argument('--context_points', type=int, default=336, help='sequence length')
+parser.add_argument('--dset', type=str, default='eeg_time', help='dataset name')
+parser.add_argument('--context_points', type=int, default=64, help='sequence length') # default=512
 parser.add_argument('--target_points', type=int, default=96, help='forecast horizon')
 parser.add_argument('--batch_size', type=int, default=64, help='batch size')
-parser.add_argument('--num_workers', type=int, default=1, help='number of workers for DataLoader')
+parser.add_argument('--num_workers', type=int, default=0, help='number of workers for DataLoader')
 parser.add_argument('--scaler', type=str, default='standard', help='scale the input data')
 parser.add_argument('--features', type=str, default='M', help='for multivariate model or univariate model')
-parser.add_argument('--use_time_features', type=int, default=0, help='whether to use time features or not')
+# parser.add_argument('--use_time_features', type=int, default=0, help='whether to use time features or not')
 # Patch
-parser.add_argument('--patch_len', type=int, default=32, help='patch length')
-parser.add_argument('--stride', type=int, default=16, help='stride between patch')
+parser.add_argument('--patch_len', type=int, default=64, help='patch length')
+parser.add_argument('--stride', type=int, default=64, help='stride between patch')
 # RevIN
 parser.add_argument('--revin', type=int, default=1, help='reversible instance normalization')
 # Model args
@@ -42,13 +42,23 @@ parser.add_argument('--d_ff', type=int, default=256, help='Tranformer MLP dimens
 parser.add_argument('--dropout', type=float, default=0.2, help='Transformer dropout')
 parser.add_argument('--head_dropout', type=float, default=0, help='head dropout')
 # Optimization args
-parser.add_argument('--n_epochs', type=int, default=20, help='number of training epochs')
+parser.add_argument('--n_epochs', type=int, default=100, help='number of training epochs')
 parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
 # model id to keep track of the number of models saved
 parser.add_argument('--model_id', type=int, default=1, help='id of the saved model')
 parser.add_argument('--model_type', type=str, default='based_model', help='for multivariate model or univariate model')
 # training
 parser.add_argument('--is_train', type=int, default=1, help='training the model')
+# adding vitaldb dataset args
+parser.add_argument('--segment_sec', type=int, default=5, help='segment length in seconds')
+parser.add_argument('--eeg_rate', type=int, default=128, help='EEG sampling rate')
+parser.add_argument('--emg_rate', type=int, default=1, help='EMG sampling rate')
+parser.add_argument('--stride_sec', type=int, default=5, help='stride length in seconds')
+parser.add_argument('--mode', type=str, default='alltrain', help='mode of the dataset, pretrain, alltrain or finetune')
+# adding some new args
+parser.add_argument('--c_in', type=int, default=1, help='number of input channels')
+parser.add_argument('--target_dim', type=int, default=1, help='number of output channels')
+parser.add_argument('--num_patch', type=int, default=10, help='number of patches') # default=4
 
 
 args = parser.parse_args()
@@ -58,20 +68,20 @@ args.save_path = 'saved_models/' + args.dset + '/patchtst_supervised/' + args.mo
 if not os.path.exists(args.save_path): os.makedirs(args.save_path)
 
 
-def get_model(c_in, args):
+def get_model(args):
     """
     c_in: number of input variables
     """
-    # get number of patches
-    num_patch = (max(args.context_points, args.patch_len)-args.patch_len) // args.stride + 1    
-    print('number of patches:', num_patch)
+    # # get number of patches
+    # num_patch = (max(args.context_points, args.patch_len)-args.patch_len) // args.stride + 1    
+    # print('number of patches:', num_patch)
     
     # get model
-    model = PatchTST(c_in=c_in,
+    model = PatchTST(c_in=args.c_in,
                 target_dim=args.target_points,
                 patch_len=args.patch_len,
                 stride=args.stride,
-                num_patch=num_patch,                
+                num_patch=args.num_patch,                
                 n_layers=args.n_layers,
                 n_heads=args.n_heads,
                 d_model=args.d_model,
@@ -89,11 +99,13 @@ def get_model(c_in, args):
 def find_lr():
     # get dataloader
     dls = get_dls(args)    
-    model = get_model(dls.vars, args)
+    # model = get_model(dls.vars, args)
+    model = get_model(args)
     # get loss
     loss_func = torch.nn.MSELoss(reduction='mean')
     # get callbacks
-    cbs = [RevInCB(dls.vars)] if args.revin else []
+    # cbs = [RevInCB(dls.vars)] if args.revin else []
+    cbs = []
     cbs += [PatchCB(patch_len=args.patch_len, stride=args.stride)]
     # define learner
     learn = Learner(dls, model, loss_func, cbs=cbs)                        
@@ -104,16 +116,18 @@ def find_lr():
 def train_func(lr=args.lr):
     # get dataloader
     dls = get_dls(args)
-    print('in out', dls.vars, dls.c, dls.len)
+    # print('in out', dls.vars, dls.c, dls.len)
     
     # get model
-    model = get_model(dls.vars, args)
+    # model = get_model(dls.vars, args)
+    model = get_model(args)
 
     # get loss
     loss_func = torch.nn.MSELoss(reduction='mean')
 
     # get callbacks
-    cbs = [RevInCB(dls.vars)] if args.revin else []
+    # cbs = [RevInCB(dls.vars)] if args.revin else []
+    cbs = []
     cbs += [
          PatchCB(patch_len=args.patch_len, stride=args.stride),
          SaveModelCB(monitor='valid_loss', fname=args.save_model_name, 
@@ -149,7 +163,8 @@ def test_func():
 if __name__ == '__main__':
 
     if args.is_train:   # training mode
-        suggested_lr = find_lr()
+        # suggested_lr = find_lr()
+        suggested_lr = args.lr
         print('suggested lr:', suggested_lr)
         train_func(suggested_lr)
     else:   # testing mode
