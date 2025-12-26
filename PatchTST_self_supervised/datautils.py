ea2189ef70cf54b2c53ca6168700d4f85a958e0f
zeahmd <zeeshanahmad10809@gmail.com>
2025-12-26 02:49:23 +0100

    updated data class, dataloader, and datautils

--- PatchTST_self_supervised/datautils.py
+++ PatchTST_self_supervised/datautils.py
@@ -32,6 +32,7 @@ def get_dls(params):
                 'eeg_rate': params.eeg_rate,
                 'emg_rate': params.emg_rate,
                 'stride_sec': params.stride_sec,
+                'debug': params.debug,
                 'mode': params.mode,
                 },
                 batch_size=params.batch_size,
--- PatchTST_self_supervised/src/data/datamodule.py
+++ PatchTST_self_supervised/src/data/datamodule.py
@@ -79,8 +79,8 @@ class DataLoaders:
             batch_size=self.batch_size,
             num_workers=self.workers,
             collate_fn=self.collate_fn,
-            # sampler=sampler
-            sampler=None
+            sampler=sampler
+            # sampler=None
         )
 
 
